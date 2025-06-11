bc540942454681fe35e159f8a7806f25e612aa88
Ulisaurio <77359543+Ulisaurio@users.noreply.github.com>
2025-06-11 15:47:07 -0600

    Fix enrolar command export

--- PanelDomoticoWeb/comandos/enrolar.mjs
+++ PanelDomoticoWeb/comandos/enrolar.mjs
@@ -1,4 +1,6 @@
 import sendSerial from '../util/sendSerial.mjs';
 
+export default async function (id) {
+    if (!id && id !== 0) throw new Error('ID de huella requerido');
     return await sendSerial(`enrolar ${id}`);
 }
